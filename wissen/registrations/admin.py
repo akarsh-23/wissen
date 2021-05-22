4a1d23ec9fa02da991f25b2e158d8435672d3c99
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2021-05-23 01:11:04 +0530

    bug fixed

--- wissen/registrations/admin.py
+++ wissen/registrations/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ['event','name','email','phone','registrationNo','college','course']
+
+admin.site.register(Registration, RegistrationAdmin)
+
--- wissen/registrations/views.py
+++ wissen/registrations/views.py
@@ -5,23 +5,41 @@ from org import models
 
 
 def home(request):
-    if request.method == "GET" and request.GET.get('Event'):
-        event = models.Event.objects.get(name=request.GET.get('Event'))
-        if event:
-            print(event.status)
-            if event.status:
-                registrationForm = RegistrationForm({'event': request.GET.get('Event')})
+    try:
+        if request.method == "GET" and request.GET.get('Event'):
+            event = models.Event.objects.get(name=request.GET.get('Event'))
+            if event:
+                if event.status:
+                    registrationForm = RegistrationForm({'event': request.GET.get('Event')})
+                    context = {
+                        'media_url' : settings.MEDIA_URL,
+                        'RegistrationForm' : registrationForm,
+                        'image' : event.image,
+                    }
+                    return render(request, 'registration/registrations.html', context=context)
+                else:
+                    registrationForm = RegistrationForm({'event': request.GET.get('Event')})
+                    context = {
+                        'media_url' : settings.MEDIA_URL,
+                        'error' : 'Registration Closed!',
+                        'image' : event.image,
+                    }
+                    return render(request, 'registration/geterror.html', context=context)
+
+        elif request.method == "POST":
+            registrationForm = RegistrationForm(request.POST)
+            event = models.Event.objects.get(name=request.POST.get('event'))
+            if registrationForm.is_valid():
+                newresponse = registrationForm.save()
+                newresponse.save()
+                return redirect('http://127.0.0.1:8000/ThankYou')
+            else:
+                registrationForm = RegistrationForm(request.POST)
                 context = {
                     'media_url' : settings.MEDIA_URL,
                     'RegistrationForm' : registrationForm,
                     'image' : event.image,
                 }
-                return render(request, 'registration/registrations.html', context=context)
-
-    elif request.method == "POST":
-        registrationForm = RegistrationForm(request.POST)
-        if registrationForm.is_valid():
-            newresponse = registrationForm.save()
-            newresponse.save()
-            return redirect('http://127.0.0.1:8000/ThankYou')
-    return redirect('http://127.0.0.1:8000')
+                return render(request, 'registration/posterror.html', context=context)
+    except:
+        return redirect('http://127.0.0.1:8000/Events')
