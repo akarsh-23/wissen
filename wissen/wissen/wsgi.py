968d0ff30a8d3d7158bf7711d74a9968fc3bd69b
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2021-06-18 13:56:12 +0530

    sixth commit

--- wissen/wissen/wsgi.py
+++ wissen/wissen/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wissen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wissen.settings.py')
 
 application = get_wsgi_application()
